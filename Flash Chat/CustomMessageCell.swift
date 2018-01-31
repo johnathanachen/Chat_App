d9896dc80b6203c7c18d4a8c4a967cc6bd82501e
Johnathan Chen <johnathanchenn@outlook.com>
2018-01-31 13:23:34 -0800

    project files

---
+++ Flash Chat/CustomMessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomMessageCell.swift
+//  Flash Chat
+//
+//  Created by Johnathan Chen on 1/26/18.
+//  Copyright © 2018 DEVOPS. All rights reserved.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
---
+++ Flash Chat/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by Johnathan Chen on 1/26/18.
+//  Copyright © 2018 DEVOPS. All rights reserved.
+//
+
+class Message {
+    
+    //TODO: Messages need a messageBody and a sender variable
+    var sender : String = ""
+    var messageBody : String = ""
+    
+    
+}
